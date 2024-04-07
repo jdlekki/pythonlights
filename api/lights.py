8f8b510fa0fe3d30eee5f9e748943d98dc81a5de
jdlekki <4064684+jdlekki@users.noreply.github.com>
2024-04-06 21:03:12 -0400

    removed unused code

--- api/lights.py
+++ api/lights.py
@@ -16,11 +16,6 @@ async def setLightColor(ip, red, green, blue):
     light = wizlight(ip)
     await light.turn_on(PilotBuilder(rgb = (red, green, blue)))
 
-async def setLightsColor(bulbList):
-    for bulb in bulbList:
-        light = wizlight(bulb.ip)
-        await light.turn_on(PilotBuilder(rgb = (bulb.RGB.red, bulb.RGB.green, bulb.RGB.blue)))
-
 
 async def main():
     bulbs = await getAllLights("192.168.254.255")
@@ -33,15 +28,3 @@ class BulbListMember:
     def __init__(self, ip, listNumber):
         self.ip = ip
         self.listNumber = listNumber
-
-class RGB:
-    def __init__(self, red, green, blue):
-        self.red = red
-        self.green = green
-        self.blue = blue
-
-class BulbListWithColor:
-    def __init__(self, ip, listNumber, RGB):
-        self.ip = ip
-        self.listNumber = listNumber
-        self.RGB = RGB
