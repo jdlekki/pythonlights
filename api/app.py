749b766e81c204d6ea473a41fd6571fb60f07603
jdlekki <4064684+jdlekki@users.noreply.github.com>
2024-04-06 20:59:27 -0400

    add light functionality for setting lights

--- api/app.py
+++ api/app.py
@@ -1,22 +1,39 @@
-from flask import Flask
-from flask import render_template
-from flask import request
+from flask import Flask, render_template, request, url_for, flash, redirect
 import lights
 import asyncio
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2decf1d53dd9c26c755be2c6702bbfe568f15bab34097cde'
 
-@app.route("/")
+lightList = asyncio.run(lights.getAllLights("192.168.254.255"))
+
+
 @app.route('/hello/<name>')
 def hello_world(name=None):
     return render_template("home.html", name=name)
 
-@app.route("/getLights")
-def getLights():
-    lightList = asyncio.run(lights.getAllLights("192.168.254.255")) 
+@app.route("/allLights", methods=('GET', 'POST'))
+def allLights():
+    if request.method == 'POST':
+        for light in lightList:
+            input = request.form[f'{light.listNumber}']
+            print(input)
+            hexColor = input.lstrip('#')
+            rgb = hexToRGB(hexColor)
+            red = rgb[0]
+            green = rgb[1]
+            blue = rgb[2]
+            asyncio.run(lights.setLightColor(light.ip, red, green, blue))
     return render_template("lightlist.html", lightList=lightList)
 
 @app.route("/color")
 def color():
     colorString = request.args.get('color', '')
     return render_template('color.html', colorString=colorString)
+
+def hexToRGB(hex):
+    rgb = []
+    for i in (0, 2, 4):
+        decimal = int(hex[i:i+2], 16)
+        rgb.append(decimal)
+    return tuple(rgb)
--- api/lights.py
+++ api/lights.py
@@ -3,18 +3,24 @@ import asyncio
 from pywizlight import wizlight, PilotBuilder, discovery
 
 async def getAllLights(network):
-    """Sample code to work with bulbs."""
-    # Discover all bulbs in the network via broadcast datagram (UDP)
-    # function takes the discovery object and returns a list of wizlight objects.
     bulbs = await discovery.discover_lights(broadcast_space=network)
     bulbList = []
+    x = 0
     for bulb in bulbs:
-        bulbList.append(bulb.ip)
-
+        x += 1
+        listMember = BulbListMember(bulb.ip,x)
+        bulbList.append(listMember)
     return bulbList
+
 async def setLightColor(ip, red, green, blue):
     light = wizlight(ip)
     await light.turn_on(PilotBuilder(rgb = (red, green, blue)))
+
+async def setLightsColor(bulbList):
+    for bulb in bulbList:
+        light = wizlight(bulb.ip)
+        await light.turn_on(PilotBuilder(rgb = (bulb.RGB.red, bulb.RGB.green, bulb.RGB.blue)))
+
 
 async def main():
     bulbs = await getAllLights("192.168.254.255")
@@ -23,3 +29,19 @@ async def main():
 
 #loop = asyncio.get_event_loop()
 #loop.run_until_complete(main())
+class BulbListMember:
+    def __init__(self, ip, listNumber):
+        self.ip = ip
+        self.listNumber = listNumber
+
+class RGB:
+    def __init__(self, red, green, blue):
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+class BulbListWithColor:
+    def __init__(self, ip, listNumber, RGB):
+        self.ip = ip
+        self.listNumber = listNumber
+        self.RGB = RGB
